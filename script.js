e29d5106df061b301c94e7551bab56462d9e5b19
sja_thedude <sja.affu765@gmail.com>
2021-10-04 18:35:26 +0300

    Updated with linter errors

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-let logo = document.querySelector('.logo');
-let icon = document.querySelector('.btn');
-let menuList = document.querySelector('.nav-ditems');
-let mainBody = document.querySelector('.mainabout');
-let menuItems = Array.from(document.querySelectorAll('.nav-ditem'));
-let menuButton = document.querySelector('.menubtn');
+const logo = document.querySelector('.logo');
+const icon = document.querySelector('.btn');
+const menuList = document.querySelector('.nav-ditems');
+const mainBody = document.querySelector('.mainabout');
+const menuItems = Array.from(document.querySelectorAll('.nav-ditem'));
+const menuButton = document.querySelector('.menubtn');
 
 const filterStyle = () => {
-    mainBody.style.filter = 'none';
-    logo.style.filter = 'none';
-  };
+  mainBody.style.filter = 'none';
+  logo.style.filter = 'none';
+};
 
-  const toggleMenu = () => {
-    menuList.classList.toggle('active');
-    menuButton.classList.toggle('active');
-  };
+const toggleMenu = () => {
+  menuList.classList.toggle('active');
+  menuButton.classList.toggle('active');
+};
   
-  menuButton.addEventListener('click', () => {
-    toggleMenu();
-  
+menuButton.addEventListener('click', () => {
+  toggleMenu();
     if (menuButton.classList.contains('active')) {
       icon.src = 'img/Icon-close.png';
       menuBody.style.filter = 'none';
@@ -26,19 +25,19 @@ const filterStyle = () => {
       filterStyle();
       window.location.reload();
     }
-  });
-  menuItems.forEach((item) => {
-    item.addEventListener('click', () => {
-      toggleMenu();
-      menuList.classList.remove('active');
-      filterStyle();
-      icon.src = 'img/Union.png';
-    });
-  });
+});
 
-  window.addEventListener('resize', () => {
-    if (mainBody.clientWidth >= 992 && menuButton.classList.contains('active')) {
-      window.location.reload();
-    }
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    toggleMenu();
+    menuList.classList.remove('active');
+    filterStyle();
+    icon.src = 'img/Union.png';
   });
-    
+});
+
+window.addEventListener('resize', () => {
+  if (mainBody.clientWidth >= 992 && menuButton.classList.contains('active')) {
+    window.location.reload();
+  }
+});
