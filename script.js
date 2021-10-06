bbc104eef09c01cb9f47f3edda054c8cb702c0c0
sja_thedude <sja.affu765@gmail.com>
2021-10-06 19:22:27 +0300

    Mobile Popup Window Added

--- script.js
+++ script.js
@@ -61,3 +61,40 @@ form.addEventListener('submit', (e) => {
     mail.style.borderRadius = '4px';
   }
 });
+
+const openPopupButtons = document.querySelectorAll('[data-popup-target]')
+const closePopupButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openPopupButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const popup = document.querySelector(button.dataset.popupTarget)
+    openPopup(popup)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const popups = document.querySelectorAll('.popup.active')
+  popups.forEach(popup => {
+    closePopup(popup)
+  })
+})
+
+closePopupButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const popup = button.closest('.popup')
+    closePopup(popup)
+  })
+})
+
+function openPopup(popup) {
+  if (popup == null) return
+    popup.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closePopup(popup) {
+  if (popup == null) return
+    popup.classList.remove('active')
+    overlay.classList.remove('active')
+}
