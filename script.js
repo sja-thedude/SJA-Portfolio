a03872cc3e4556c87161f28d676fb75bdf443d67
sja_thedude <sja.affu765@gmail.com>
2021-10-07 08:38:43 +0300

    Files Updated For Popup

--- script.js
+++ script.js
@@ -79,22 +79,6 @@ const data = [
     live_version: 'https://sja-thedude.github.io/',
     source_code: 'https://github.com/sja-thedude/',
   },
-  {
-    pro: 'Multi-Post Stories',
-    proimg: 'img/snapchat-small.png',
-    prodes: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
-    prolang: ['html', 'Ruby on rails', 'css'],
-    live_version: 'https://sja-thedude.github.io/',
-    source_code: 'https://github.com/sja-thedude/',
-  },
-  {
-    pro: 'Multi-Post Stories',
-    proimg: 'img/snapchat-small.png',
-    prodes: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
-    prolang: ['html', 'Ruby on rails', 'css'],
-    live_version: 'https://sja-thedude.github.io/',
-    source_code: 'https://github.com/sja-thedude/',
-  },
 ];
 
 const sec = document.querySelector('.popup');
