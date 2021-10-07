6f479ccc01b22e95f72398a98fe60b8fe9f87da6
sja_thedude <sja.affu765@gmail.com>
2021-10-07 08:45:47 +0300

    Updated JS File

--- script.js
+++ script.js
@@ -73,9 +73,9 @@ const data = [
   },
   {
     pro: 'Multi-Post Stories',
-    proimg: 'img/snapchat-small.png',
+    proimg: 'img/Snapshoot-big.png',
     prodes: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
-    prolang: ['html', 'Ruby on rails', 'css'],
+    prolang: ['html', 'Ruby on rails', 'css', 'Github'],
     live_version: 'https://sja-thedude.github.io/',
     source_code: 'https://github.com/sja-thedude/',
   },
